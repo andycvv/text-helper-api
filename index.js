0a34f5f3f7d4c427fe24a2805194f133f7b0b777
andycvv <135892408+andycvv@users.noreply.github.com>
2024-08-06 10:51:50 -0500

    fix port and clean up unnecessary code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT
 app.disable('x-powered-by')
 
 const groq = createOpenAI({
